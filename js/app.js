827dd5925de8eb6c0015677d0a51174684422d6f
Sam Hamm <sam@codefellows.com>
2019-05-01 14:57:22 -0700

    End of demo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-alert('IT IS ALIVE');
+'use strict';
+
+var userName = prompt('Hi! Welcome! I\'m Sam! What is your name?');
+alert('Hi, ' + userName + '! I\'m glad you are here! Let\'s play a guessing game so that you can learn more about me.');
+console.log('The user\'s name is '+ userName);
+
+var answerOne = prompt('Guess this about me: have I ever been on a vacation to Key West?').toLowerCase();
+
+if (answerOne === 'y' || answerOne === 'yes'){
+  alert('You\'re right, ' + userName + '! In fact, I am going back there in about 4 weeks!');
+  console.log('User got the correct answer to #1');
+} else {
+  alert('Sorry, ' + userName + ', that is incorrect. I was last there in July 2018');
+  console.log('User got the incorrect answer to #1');
+}
+
